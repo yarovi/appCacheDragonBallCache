418f68922e26e1030b268e317937f5b083d0461e
Yasmani Rosado <tas185@gmail.com>
2025-04-14 21:44:03 -0500

    initial configuration

---
+++ src/main/java/org/yasmani/io/appcachedragonballcache/AppCacheDragonBallCacheApplication.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.appcachedragonballcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCacheDragonBallCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCacheDragonBallCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yasmani/io/appcachedragonballcache/AppCacheDragonBallCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.appcachedragonballcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCacheDragonBallCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
